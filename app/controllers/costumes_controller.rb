a7a44b97f143f23af8950271102569e96d7b2f33
bambi teague <teeegsa@gmail.com>
2021-03-31 14:43:59 -0500

    small tweaks

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -2,10 +2,11 @@ class CostumesController < ApplicationController
   # READ all costumes
   # right now this is boring/not useful. would like to turn into a part of the client's "homepage" that lets them view their current projects with me
   # alternatively, showing them all my current projects and the status of their order in my que of projects?
+
   get '/costumes' do
     redirect_if_not_logged_in
     @costumes = Costume.all
-    erb :'costumes/index' 
+    erb :'costumes/gallery' 
   end
 
   # CREATE new costume (render form)
@@ -14,24 +15,24 @@ class CostumesController < ApplicationController
     erb :'costumes/new'
   end
 
+  # CREATE new costume (save in DB)
+  post '/costumes' do
+    redirect_if_not_logged_in
+    costumes = Costume.new(params["name"]["description"])
+    costumes.user_id = session["user_id"]
+    if costumes.save
+      redirect "/homepage/my_home"
+    else
+      redirect "costumes/new"
+    end
+  end
+
   # READ 1 costume
   get '/costumes/:id' do
     redirect_if_not_logged_in
     redirect_if_not_authorized
     erb :'costumes/show'
   end 
-
-  # CREATE new costume (save in DB)
-  post '/costumes' do
-    redirect_if_not_logged_in
-    costumes = Costume.new(params["name"])
-    costumes.user_id = session["user_id"]
-    if costumes.save
-      redirect "/costumes/#{costumes.id}"
-    else
-      redirect "costumes/new"
-    end
-  end
 
   # UPDATE 1 costume (render form)
   get '/costumes/:id/edit' do
