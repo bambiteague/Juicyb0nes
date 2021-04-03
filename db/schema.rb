0f7f3db9ee0d67bf20b9a01e532c5be2d5f5756c
bambi teague <teeegsa@gmail.com>
2021-04-03 10:42:16 -0500

    migration to remove age from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_153854) do
+ActiveRecord::Schema.define(version: 2021_04_03_154106) do
 
   create_table "costumes", force: :cascade do |t|
     t.text "name"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2021_04_03_153854) do
 
   create_table "users", force: :cascade do |t|
     t.text "name"
-    t.integer "age"
     t.string "email"
     t.string "password_digest"
     t.text "reviews"
